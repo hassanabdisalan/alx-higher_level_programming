6a3f40488a0ad6c64b8ef5b24078ae5e459ad938
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-09-10 11:10:56 -0700

    task 14 advanced

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+def print_tebahpla():
+    """Print the ASCII alphabet in reverse order with alternating cases."""
+    for i in range(25, -1, -1):  # Start from 25 (z) to 0 (a)
+        char = chr(122 - i)  # 122 is the ASCII value for 'z'
+        if i % 2 == 0:
+            print(char, end="")
+        else:
+            print(char.upper(), end="")
