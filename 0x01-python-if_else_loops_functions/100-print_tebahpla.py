fc2aa2140e5cee8998d0cfd66fce114e67ca8073
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-09-10 11:20:43 -0700

    task 14 advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_tebahpla():
-    """Print the ASCII alphabet in reverse order with alternating cases."""
-    print(''.join(chr(122 - i) if i % 2 == 0 else chr(90 - i + 32) for i in range(26)))
-    print_tebahpla()
+for i in range(122, 96, -1):
+    if (i % 2) != 0:
+        i -= 32
+    print("{}".format(chr(i)), end="")
+    if (i % 2) != 0:
+        i += 32
