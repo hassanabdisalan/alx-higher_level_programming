197d7dc3c0d924402d9a50670e9aeb8264767bac
hassanabidsalan <hassanabdisalan3@gmail.com>
2024-10-02 15:28:35 +0300

    task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-"""defines a square"""
+"""module containing square class"""
 
 
 class Square:
-	""" square with private instance attributes size"""
+    """representing a square"""
 
-	def __init__(self, size=0):
-		"""
-		initializes square
-		Args:
-		size: size of side square
-		"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-		if type(size) is int:
-			if size < 0:
-				raise ValueError('size must be >= 0')
-			else:
-				self.__size = size
-		else:
-			raise TypeError('size must be an integer')
-		
-		def area(sefl):
-			"""
-			finds area os square
-			Returns: 
-				the area of the square
-			"""
-			return self.__size ** 2
-		
+    def area(self):
+        """return the square area"""
+
+        return (self.__size)**2
