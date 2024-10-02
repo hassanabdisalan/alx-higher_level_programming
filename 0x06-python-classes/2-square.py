fffa3ee5c13caa2858214d38c994d7eee701b392
hassanabidsalan <hassanabdisalan3@gmail.com>
2024-10-02 15:26:01 +0300

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/pyhton3
+"""define a square"""
+
+
+class Square:
+	""" square with private intance attributes size"""
+
+	def __init__(self, size=0):
+		"""
+		Args:
+			size: size of square
+		"""
+
+		if type(size) is int:
+			if size < 0:
+				raise ValueError("size must be >= 0")
+			else:
+				self.__size = size
+		else:
+			raise TypeError("size must be an integer")
+		
