42da52e789e2b182b556703024fa9d7d9f09410a
hassanabidsalan <hassanabdisalan3@gmail.com>
2024-10-14 19:20:29 +0300

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Contains the lookup function
+"""
+
+
+def lookup(object):
+	"""Returns the list of available attributes and methods of an object"""
+	return dir(object)
+
+
+
+
+
+
+
+
+
+
+
