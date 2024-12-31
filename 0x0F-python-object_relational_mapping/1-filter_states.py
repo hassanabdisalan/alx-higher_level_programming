bcd5e9ba27b872c7ab0bb9a14e2f4547dfbb9874
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-12-31 09:18:28 +0300

    Task 0 and 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+Listing all states from the database hbtn_0e_0_usa
+"""
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
+def main():
+    """
+    List 'states' table of 'hbtn_0e_0_usa' database in ascending
+    order by id's
+    """
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY id;")
+    rows = cursor.fetchall()
     for row in rows:
-        print(row)
-    cur.close()
+        if ("N" in row[1]):
+            print(row)
+
+    cursor.close()
     db.close()
-    
+
+
+if __name__ == '__main__':
+    main()
