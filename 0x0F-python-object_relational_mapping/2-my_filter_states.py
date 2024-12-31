744776643d652af1c18a93d7013894336d7a3a35
hassanabdisalan <hassanabdisalan3@gmail.com>
2024-12-31 09:24:02 +0300

    task 2 debug

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+Listing all states from the database hbtn_0e_0_usa
+"""
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
-    rows = cur.fetchall()
+def main():
+    """
+    List 'states' table of 'hbtn_0e_0_usa' database in ascending
+    order by id's
+    """
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+
+    query = """SELECT * FROM states WHERE name LIKE '{}' ORDER BY
+    id;""".format(argv[4])
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
     for row in rows:
-        print(row)
-    cur.close()
+        if (row[1] == argv[4]):
+            print(row)
+
+    cursor.close()
     db.close()
+
+
+if __name__ == '__main__':
+    main()
