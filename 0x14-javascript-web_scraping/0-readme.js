1903ee882a8c2c37aa3ef6cdd066b63ede5eebcd
hassanabdisalan <hassanabdisalan3@gmail.com>
2025-02-11 18:39:27 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-	console.log(err || data);
-})
+
+const filePath = process.argv[2]; // Get the file path from the command-line argument
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
